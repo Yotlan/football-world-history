54b2fbd7620583d806c9026328ade31508b1cb72
e184058n <yotlan.le-crom@etu.univ-nantes.fr>
2022-05-19 14:17:10 +0200

    Fix 1924-1925 squad file

--- scripts/python/convert_squad_file.py
+++ scripts/python/convert_squad_file.py
@@ -6,9 +6,9 @@ import pandas as pd
 import re
 
 #Select directories
-dirs = glob.glob('../../csv/**/**/Football_League_First_Division/equipes/1923-1924')
+dirs = glob.glob('../../csv/**/**/Football_League_First_Division/equipes/1924-1925')
 #Select files
-files = glob.glob('../../csv/**/**/Football_League_First_Division/equipes/1923-1924/*.csv')
+files = glob.glob('../../csv/**/**/Football_League_First_Division/equipes/1924-1925/*.csv')
 
 def produce():
     print('Start...')
