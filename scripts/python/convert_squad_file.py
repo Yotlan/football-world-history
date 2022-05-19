79ffc4a145e51c7dc05eafd5a4d58ecbf5fe202c
e184058n <yotlan.le-crom@etu.univ-nantes.fr>
2022-05-19 12:37:29 +0200

    Fix 1914-1915 squad file

--- scripts/python/convert_squad_file.py
+++ scripts/python/convert_squad_file.py
@@ -6,9 +6,9 @@ import pandas as pd
 import re
 
 #Select directories
-dirs = glob.glob('../../csv/**/**/Football_League_First_Division/equipes/1913-1914')
+dirs = glob.glob('../../csv/**/**/Football_League_First_Division/equipes/1914-1915')
 #Select files
-files = glob.glob('../../csv/**/**/Football_League_First_Division/equipes/1913-1914/*.csv')
+files = glob.glob('../../csv/**/**/Football_League_First_Division/equipes/1914-1915/*.csv')
 
 def produce():
     print('Start...')
