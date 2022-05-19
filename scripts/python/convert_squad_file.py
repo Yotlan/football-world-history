34ce1d5dfc9ca45fa87189b1014948d77c9c9c4f
e184058n <yotlan.le-crom@etu.univ-nantes.fr>
2022-05-19 11:16:11 +0200

    Fix 1910-1911 squad file

--- scripts/python/convert_squad_file.py
+++ scripts/python/convert_squad_file.py
@@ -6,9 +6,9 @@ import pandas as pd
 import re
 
 #Select directories
-dirs = glob.glob('../../csv/**/**/Football_League_First_Division/equipes/1909-1910')
+dirs = glob.glob('../../csv/**/**/Football_League_First_Division/equipes/1910-1911')
 #Select files
-files = glob.glob('../../csv/**/**/Football_League_First_Division/equipes/1909-1910/*.csv')
+files = glob.glob('../../csv/**/**/Football_League_First_Division/equipes/1910-1911/*.csv')
 
 def produce():
     print('Start...')
