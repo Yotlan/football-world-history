a2e061cada24fde6c9b131413503ce9b4c61425e
e184058n <yotlan.le-crom@etu.univ-nantes.fr>
2022-05-19 15:17:31 +0200

    Fix 1927-1928

--- scripts/python/convert_squad_file.py
+++ scripts/python/convert_squad_file.py
@@ -6,9 +6,9 @@ import pandas as pd
 import re
 
 #Select directories
-dirs = glob.glob('../../csv/**/**/Football_League_First_Division/equipes/1926-1927')
+dirs = glob.glob('../../csv/**/**/Football_League_First_Division/equipes/1927-1928')
 #Select files
-files = glob.glob('../../csv/**/**/Football_League_First_Division/equipes/1926-1927/*.csv')
+files = glob.glob('../../csv/**/**/Football_League_First_Division/equipes/1927-1928/*.csv')
 
 def produce():
     print('Start...')
